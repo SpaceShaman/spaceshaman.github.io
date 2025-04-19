a372e9eece000fd6f62800381f4ea06d6ed1d60f
SpaceShaman <spaceshaman@tuta.io>
2025-04-19 08:08:30 +0200

    ✨ (Experiances.vue): add new component to display work experiences ✨ (RightSection.vue): integrate Experiances component into RightSection ✨ (experiances.json): add data file for work experiences

---
+++ src/components/RightSection/Experiances.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+	import experiances from '~/data/experiances.json'
+</script>
+<template>
+	<Card title="Work Experience">
+		<CardItem
+			v-for="(experiance, index) in experiances"
+			:key="index"
+			:title="experiance.title"
+			:subtitle="`${experiance.startDate} - ${experiance.endDate}`"
+			:url="experiance.url"
+			:description="experiance.description"
+		/>
+	</Card>
+</template>
--- src/components/RightSection/RightSection.vue
+++ src/components/RightSection/RightSection.vue
@@ -2,6 +2,7 @@
 	<div class="flex flex-col gap-4 print:gap-2">
 		<RightSectionAboutMe />
 		<RightSectionProjects />
+		<RightSectionExperiances />
 		<RightSectionEducation />
 	</div>
 </template>
